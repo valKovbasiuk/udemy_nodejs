7a83ec9adc3b06c4178135a78cb38a63224867e2
Valentyn <valentyn@paymentwalls-MacBook-Pro.local>
2020-01-29 16:59:25 +0200

    initial commit

---
+++ section-03/app.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+const server = http.createServer((request, response) => {
+  console.log(request.url, request.method, request.headers);
+  // process.exit();
+  response.setHeader('Content-Type', 'text/html');
+  response.write('<html>');
+  response.write('<body> hello </body>');
+  response.write('</html');
+  response.end();
+});
+
+server.listen(3000);
