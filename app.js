d292d8f74057205d035de7ccd65d42a85b27a680
Valentyn <valentyn@paymentwalls-MacBook-Pro.local>
2020-01-31 17:57:37 +0200

    save before asignment 02

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-const http = require('http');
+const express = require('express');
 
-const server = http.createServer((request, response) => {
-  console.log(request.url, request.method, request.headers);
-  // process.exit();
-  response.setHeader('Content-Type', 'text/html');
-  response.write('<html>');
-  response.write('<body> hello </body>');
-  response.write('</html');
-  response.end();
+const app = express();
 
+app.use('/add-product',(req, res, next) => {
+  console.log('in another middleware');
+  res.send('<h1>add product page</h1>');
 });
 
-server.listen(3000);
+app.use('/',(req, res, next) => {
+  console.log('in another middleware');
+  res.send('<h1>hello from express</h1>');
+});
+
+app.listen(3000);
